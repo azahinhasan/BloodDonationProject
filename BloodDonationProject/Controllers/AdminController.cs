4f276c31c2a90f015868d29a4d8906c70b84e43b
A H Zahin <azahinhasan@gmail.com>
2021-03-14 01:44:46 +0600

    chart added

--- BloodDonationProject/Controllers/AdminController.cs
+++ BloodDonationProject/Controllers/AdminController.cs
@@ -281,55 +281,6 @@ namespace BloodDonationProject.Controllers
             return View();
         }
 
-        public ActionResult Bar()
-        {
-            return View();
-        }
-
-        public class Part 
-        {
-            public string Employee { get; set; }
-
-            public int Credit { get; set; }
-        }
-
-            [HttpPost]
-        public JsonResult NewChart()
-        {
-            List<Part> iData = new List<Part>();
-            //Creating sample data  
-            //DataTable dt = new DataTable();
-            /*        dt.Columns.Add("Employee", System.Type.GetType("System.String"));
-                    dt.Columns.Add("Credit", System.Type.GetType("System.Int32"));
-
-                    DataRow dr = dt.NewRow();
-                    dr["Employee"] = "Sam";
-                    dr["Credit"] = 123;
-                    dt.Rows.Add(dr);
-
-                    dr = dt.NewRow();
-                    dr["Employee"] = "Alex";
-                    dr["Credit"] = 456;
-                    dt.Rows.Add(dr);
-
-                    dr = dt.NewRow();
-                    dr["Employee"] = "Michael";
-                    dr["Credit"] = 587;
-                    dt.Rows.Add(dr);*/
-
-            //Looping and extracting each DataColumn to List<Object>  
-            /*   foreach (DataColumn dc in dt.Columns)
-               {
-                   List<object> x = new List<object>();
-                   x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
-                   iData.Add(x);
-               }*/
-            //Source data returned as JSON  
-            iData.Add(new Part() { Employee = "crank arm", Credit = 123 });
-            iData.Add(new Part() { Employee = "caaa", Credit = 523 });
-
-            return Json(iData, JsonRequestBehavior.AllowGet);
-        }
 
     }
 
